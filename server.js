ed2670542beea0035ac94dd43ca94c53ece255c3
bilgey-zey <p7p2j7ckbp@privaterelay.appleid.com>
2026-03-01 01:49:11 +0300

    Use environment variable for server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 import pool from "./db.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
